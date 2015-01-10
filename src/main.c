60fbdcad97d6d2f2eee678242f0b0b65a7281427
Craig Hu <cryhu@ucsc.edu>
2015-01-10 13:16:08 -0800

    initial test
    test animation

--- src/main.c
+++ src/main.c
@@ -1,26 +1,29 @@
-#include <pebble.h>
-  
-#include "message.h"
-#include "face.h"
-#include "status.h"
-#include "input.h"
-#include "notification.h"
-  
-Window *window_main;
+#include "pebble.h"
+
+Window *window;
 TextLayer *text_time_layer;
 
+
+// There are two of these because we have to schedule the next animation
+// while the other is still in motion because otherwise we cancel the
+// in-progress animation.
+// (Actually, with the current implementation this is probably no longer
+// necessary.)
 static PropertyAnimation *up_animation;
 static PropertyAnimation *down_animation;
 
+
+// This works around the inability to use the current GRect macro
+// for constants.
 #define ConstantGRect(x, y, w, h) {{(x), (y)}, {(w), (h)}}
 
 GRect UPPER_TO_RECT = ConstantGRect(0, 5, 144, 168-5);
 GRect LOWER_TO_RECT = ConstantGRect(0, 112, 144, 168-112);
 
+
 #define ANIMATION_DURATION_IN_MS 1500
-  
+
 static void init_animations() {
-  
   Layer *layer = text_layer_get_layer(text_time_layer);
 
   up_animation = property_animation_create_layer_frame(layer, &LOWER_TO_RECT, &UPPER_TO_RECT);
@@ -56,9 +59,9 @@ static void schedule_animation(struct tm *tick_time) {
 
 }
 
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 
-static void tick_handler_test(struct tm *tick_time, TimeUnits units_change) {
-  
+  // Need to be static because it's used by the system later.
   static char time_text[] = "00:00";
 
   strftime(time_text, sizeof(time_text), "%R", tick_time);
@@ -67,52 +70,29 @@ static void tick_handler_test(struct tm *tick_time, TimeUnits units_change) {
   schedule_animation(tick_time);
 }
 
-void window_load(Window *window) {
-  face_init(window);
-  status_init(window);
-}
-
-void window_unload(Window *window) {
-  status_deinit();
-}
-
-void init() {
-   // Open AppMessage
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-  window_main = window_create();
-  
-  window_set_window_handlers(window_main, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload
-  });
-  
-  window_set_background_color(window_main, GColorBlack);
+static void init(void) {
+  window = window_create();
+  window_stack_push(window, true /* Animated */);
+  window_set_background_color(window, GColorBlack);
 
   text_time_layer = text_layer_create(UPPER_TO_RECT);
   text_layer_set_text_color(text_time_layer, GColorWhite);
   text_layer_set_background_color(text_time_layer, GColorClear);
   text_layer_set_text_alignment(text_time_layer, GTextAlignmentCenter);
-  text_layer_set_font(text_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  layer_add_child(window_get_root_layer(window_main), text_layer_get_layer(text_time_layer));
+  text_layer_set_font(text_time_layer,
+      fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_CONDENSED_SUBSET_40)));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_time_layer));
 
   init_animations();
-  
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler_test);
-  
-  input_init(window_main);
-  initNotification();
-  
-  window_stack_push(window_main, true);
+  tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
 }
 
-void deinit() {
+static void deinit(void) {
   property_animation_destroy(up_animation);
   property_animation_destroy(down_animation);
 
   text_layer_destroy(text_time_layer);
-  
-  input_deinit();
-  window_destroy(window_main);
+  window_destroy(window);
 }
 
 int main(void) {
