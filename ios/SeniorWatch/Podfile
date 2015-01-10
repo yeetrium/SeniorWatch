1403e7920f0b825be5ae24e08a041648f2d82d56
Nestor Zepeda <NestorDZepeda@gmail.com>
2015-01-09 17:14:43 -0800

    initial commit of iOS app

---
+++ ios/SeniorWatch/Podfile
@@ -0,0 +1,4 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.1'
+pod 'Parse', '~> 1.5'
+pod 'PebbleKit'
---
+++ ios/SeniorWatch/SeniorWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SeniorWatch
+//
+//  Created by Nestor Zepeda on 1/9/15.
+//  Copyright (c) 2015 Nestor Zepeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
