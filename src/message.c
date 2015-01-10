be98ef84da6aeb8da02fd619ef5646acebaf33c9
Jonathan Lu <jonathanlu321@gmail.com>
2015-01-09 20:17:03 -0800

    MOARText

--- src/message.c
+++ src/message.c
@@ -5,7 +5,9 @@ TextLayer *text_layer_message;
 void message_init(Window *window) {
   text_layer_message = text_layer_create(GRect(0, 0, 144, 168));
   text_layer_set_background_color(text_layer_message, GColorClear);
+  text_layer_set_font(text_layer_message, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_color(text_layer_message, GColorBlack);
+  text_layer_set_text_alignment(text_layer_message, GTextAlignmentCenter);
   
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer_message));
 }
